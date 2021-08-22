96eecc56825118cd0cfcfd98deb741937224cd14
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2021-08-22 14:16:03 +0200

    use token-based auth for uploads as HTTP Basic auth does not seem to work with Axis camera (does not send credentials)

--- src/AxisCameraUpload.cpp
+++ src/AxisCameraUpload.cpp
@@ -14,7 +14,7 @@
 #include "Poco/Net/HTTPServerRequest.h"
 #include "Poco/Net/HTTPServerResponse.h"
 #include "Poco/Net/HTTPServerParams.h"
-#include "Poco/Net/HTTPBasicCredentials.h"
+#include "Poco/Net/HTMLForm.h"
 #include "Poco/Net/ServerSocket.h"
 #include "Poco/Util/ServerApplication.h"
 #include "Poco/Util/Option.h"
@@ -29,6 +29,7 @@
 #include "Poco/FileStream.h"
 #include "Poco/Path.h"
 #include "Poco/File.h"
+#include "Poco/URI.h"
 #include <sstream>
 #include <iostream>
 
@@ -36,51 +37,7 @@
 using namespace std::string_literals;
 
 
-class BasicRequestHandler: public Poco::Net::HTTPRequestHandler
-{
-public:
-	void ignoreContent(Poco::Net::HTTPServerRequest& request)
-	{
-		Poco::NullOutputStream nullStream;
-		Poco::StreamCopier::copyStream(request.stream(), nullStream);
-	}
-
-	static bool authenticate(const Poco::Net::HTTPServerRequest& request, const std::string& username, const std::string& password)
-	{
-		if (request.hasCredentials())
-		{
-			std::string scheme;
-			std::string authInfo;
-			request.getCredentials(scheme, authInfo);
-			Poco::Net::HTTPBasicCredentials creds(authInfo);
-			return creds.getUsername() == username && creds.getPassword() == password;
-		}
-		else return false;
-	}
-
-	static void sendResponse(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPResponse::HTTPStatus status, const std::string& message)
-	{
-		request.response().setContentType("text/html"s);
-		request.response().setStatusAndReason(status);
-
-		std::string html("<!DOCTYPE html>\n<html><head><title>");
-		html += Poco::NumberFormatter::format(static_cast<int>(status));
-		html += " - ";
-		html += request.response().getReasonForStatus(status);
-		html += "</title></head><body><header><h1>"s;
-		html += Poco::NumberFormatter::format(static_cast<int>(status));
-		html += " - ";
-		html += request.response().getReasonForStatus(status);
-		html += "</h1></header><section><p>"s;
-		html += Poco::Net::htmlize(message);
-		html += "</p></section>"s;
-		html += "</body></html>"s;
-		request.response().sendBuffer(html.data(), html.size());
-	}
-};
-
-
-class ImageUploadRequestHandler: public BasicRequestHandler
+class ImageUploadRequestHandler: public Poco::Net::HTTPRequestHandler
 {
 public:
 	void handleRequest(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
@@ -92,16 +49,26 @@ public:
 		{
 			if (request.getMethod() == Poco::Net::HTTPRequest::HTTP_POST)
 			{
-				if (request.getContentType() == "image/jpeg")
+				if (authorize(request, config.getString("upload.token"s, ""s)))
 				{
-					std::string path = storeImage(request, config.getString("upload.path"s, Poco::Path::current()));
-					app.logger().information("Image stored to '%s'."s, path);
-					return sendResponse(request, Poco::Net::HTTPResponse::HTTP_OK, "Image accepted"s);
+					if (request.getContentType() == "image/jpeg")
+					{
+						std::string path = storeImage(request, config.getString("upload.path"s, Poco::Path::current()));
+						app.logger().information("Image stored to '%s'."s, path);
+						return sendResponse(request, Poco::Net::HTTPResponse::HTTP_OK, "Image accepted"s);
+					}
+					else
+					{
+						app.logger().warning("Invalid or missing content type '%s' for request from %s: %s %s"s, request.getContentType(), request.clientAddress().toString(), request.getMethod(), request.getURI());
+						ignoreContent(request);
+						return sendResponse(request, Poco::Net::HTTPResponse::HTTP_BAD_REQUEST, "Unexpected content type"s);
+					}
 				}
 				else
 				{
+					app.logger().warning("Invalid or missing token for request from %s: %s %s"s, request.clientAddress().toString(), request.getMethod(), request.getURI());
 					ignoreContent(request);
-					return sendResponse(request, Poco::Net::HTTPResponse::HTTP_BAD_REQUEST, "Unexpected content type"s);
+					return sendResponse(request, Poco::Net::HTTPResponse::HTTP_BAD_REQUEST, "Missing or invalid upload token"s);
 				}
 			}
 			else if (request.getMethod() == Poco::Net::HTTPRequest::HTTP_GET)
@@ -126,6 +93,14 @@ public:
 				sendResponse(request, Poco::Net::HTTPResponse::HTTP_INTERNAL_SERVER_ERROR, "error uploading file");
 			}
 		}
+	}
+
+	bool authorize(Poco::Net::HTTPServerRequest& request, const std::string& token)
+	{
+		Poco::URI uri(request.getURI());
+		Poco::Net::HTMLForm params;
+		params.read(uri.getRawQuery());
+		return params.get("token"s, ""s) == token;
 	}
 
 	std::string storeImage(Poco::Net::HTTPServerRequest& request, const std::string& uploadPath)
@@ -154,7 +129,8 @@ public:
 
 	std::string uploadSite(const Poco::Net::HTTPServerRequest& request) const
 	{
-		Poco::Path p(request.getURI(), Poco::Path::PATH_UNIX);
+		Poco::URI uri(request.getURI());
+		Poco::Path p(uri.getPath(), Poco::Path::PATH_UNIX);
 		p.makeDirectory();
 		if (p.depth() >= 2)
 		{
@@ -179,19 +155,31 @@ public:
 			return "defaultCamera";
 		}
 	}
-};
 
-
-class UnauthorizedRequestHandler: public BasicRequestHandler
-{
-	void handleRequest(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
+	void ignoreContent(Poco::Net::HTTPServerRequest& request)
 	{
-		auto& app = Poco::Util::Application::instance();
+		Poco::NullOutputStream nullStream;
+		Poco::StreamCopier::copyStream(request.stream(), nullStream);
+	}
 
-		app.logger().warning("Unauthenticated request from %s: %s %s"s, request.clientAddress().toString(), request.getMethod(), request.getURI());
-		ignoreContent(request);
-		response.requireAuthentication("ImageUpload");
-		response.send();
+	static void sendResponse(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPResponse::HTTPStatus status, const std::string& message)
+	{
+		request.response().setContentType("text/html"s);
+		request.response().setStatusAndReason(status);
+
+		std::string html("<!DOCTYPE html>\n<html><head><title>");
+		html += Poco::NumberFormatter::format(static_cast<int>(status));
+		html += " - ";
+		html += request.response().getReasonForStatus(status);
+		html += "</title></head><body><header><h1>"s;
+		html += Poco::NumberFormatter::format(static_cast<int>(status));
+		html += " - ";
+		html += request.response().getReasonForStatus(status);
+		html += "</h1></header><section><p>"s;
+		html += Poco::Net::htmlize(message);
+		html += "</p></section>"s;
+		html += "</body></html>"s;
+		request.response().sendBuffer(html.data(), html.size());
 	}
 };
 
@@ -212,15 +200,7 @@ public:
 			app.logger().debug("Request details: %s"s, sstr.str());
 		}
 
-		if (BasicRequestHandler::authenticate(request, config.getString("upload.username"s), config.getString("upload.password"s)))
-		{
-			return new ImageUploadRequestHandler;
-		}
-		else
-		{
-			request.response().setStatusAndReason(Poco::Net::HTTPResponse::HTTP_UNAUTHORIZED);
-			return new UnauthorizedRequestHandler;
-		}
+		return new ImageUploadRequestHandler;
 	}
 };
 
@@ -283,7 +263,6 @@ protected:
 		if (!_showHelp)
 		{
 			Poco::UInt16 port = static_cast<Poco::UInt16>(config().getInt("http.port"s, 9980));
-
 			Poco::Net::ServerSocket svs(port);
 			Poco::Net::HTTPServer srv(new ImageUploadRequestHandlerFactory, svs, new Poco::Net::HTTPServerParams);
 			srv.start();
